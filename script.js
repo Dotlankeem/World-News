dfca130efdc8a4b496176fa7dae1a89efd8222d2
Dotlankeem <dotlank79@yahoo.com>
2023-12-04 15:06:42 -1200

    changed api

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-let newsType = document.getElementById('newstype')
-let boton = document.getElementById('buton')
-let searchNews = document.getElementById('newsQuery')
-boton.addEventListener('click',function(e){
-const Api = `https://api.worldnewsapi.com/search-news?api-key=17e5dc73a9ec46858b5db2e51f2aaf2d&text=${searchNews.value}`
-fetch(Api).then(result=>result.json()).then((data)=>{
-    console.log(data)
-    let allNews;
-    data.news.map((value,index,array)=>{
-        newsType.innerHTML +=`
+let newsType = document.getElementById("newstype");
+let boton = document.getElementById("buton");
+let searchNews = document.getElementById("newsQuery");
+function allData(){
+      const Api = `https://newsapi.org/v2/top-headlines?country=us&category=business&apikey=5fd73c133fcb4bad993cef14f9943c3b`;
+      fetch(Api)
+        .then((result) => result.json())
+        .then((data) => {
+          console.log(data.articles);
+          let allNews;
+          data.articles.map((value, index, array) => {
+            newsType.innerHTML += `
         <div class="col-lg-4">
-        <img src="${value.image}" alt="" style="width:100%;height:200px" class=good">
-        <h3>Title:${value.title.slice(0,100)}</h3>
-      <small>Details:${value.title.slice(0,500)}</small> 
+        <img src="${
+          value.urlToImage
+        }" alt="" style="width:100%;height:200px" class=good">
+        <h3>Title:${value.title.slice(0, 100)}</h3>
+      <small>Details:${value.description}</small> 
+      <small>Details:${value.content}</small>
       </div>
-       `
-    })
-})
-})
+       `;
+          });
+        });
+
+}
+allData()
